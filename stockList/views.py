c0e78ce59f13292646aed926fc1d999b05e8ec4b
Nicolas <cariaganicolas8@gmail.com>
2024-11-11 16:32:34 -0300

    agregados botones del paginador

--- stockList/views.py
+++ stockList/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 
 def tables(request):
     tablesEntries=Tables.objects.all()
-    paginator = Paginator(tablesEntries, 20)
+    paginator = Paginator(tablesEntries, 5)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     return render(request, "stockList/tables.html",
