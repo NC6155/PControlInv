bbe597308819a6de3ce2af7e9386e7f7062e2084
ZicKyv <jroapizarro@gmail.com>
2024-12-06 08:59:49 -0300

    act login sin funcionar

--- core/views.py
+++ core/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def login(request):
+    return render(request, "core/login.html")
+
+def register(request):
+    return render(request, "core/register.html")
+    
 def index(request):
     return render(request, "core/index.html")
