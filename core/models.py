6043dbe53386b63f271cb26d386b7fc39fbde226
Nicolas <cariaganicolas8@gmail.com>
2024-12-09 18:36:43 -0300

    intento de validar tipo de usuario

--- core/models.py
+++ core/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-tiposUsuario=(("Bodeguero", "Bodeguero"), ("Jefe de Inventario","Jefe de Inventario"))
+
 
 class UserClass(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #crea una variable usuario que actúa como foránea del usuario principal
+    tiposUsuario=(("Bodeguero", "Bodeguero"), ("Jefe de Inventario","Jefe de Inventario"))
     ocupacion=models.TextField(max_length=20, blank=False, null=False, choices=tiposUsuario, verbose_name="Ocupación") #Crea la variable de la ocupación del usuario
+
 
     class Meta:
         verbose_name="Ocupación de usuario"
