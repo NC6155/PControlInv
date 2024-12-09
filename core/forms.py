64979415289f8258ae8f2543023e3b520fd87072
Nicolas <cariaganicolas8@gmail.com>
2024-12-09 14:59:05 -0300

    creado modelo de clases de usuario

--- core/forms.py
+++ core/forms.py
@@ -2,17 +2,19 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+tiposUsuario=(("Bodeguero", "Bodeguero"), ("Jefe de Inventario","Jefe de Inventario"))
 class CustomUserCreationForm(UserCreationForm):
 	username=forms.CharField(label="Ingrese su nombre de usuario" ,required=True, widget=forms.TextInput(attrs={"class":"form-control form-control-user"}))
 	email = forms.EmailField(label="Ingrese su Email", required=True, widget=forms.EmailInput(attrs={"class":"form-control form-control-user", "aria-describedby":"emailHelp"}))
 	first_name=forms.CharField(label="Ingrese su primer nombre",required=True, widget=forms.TextInput(attrs={"class":"form-control form-control-user"}))
 	last_name=forms.CharField(label="Ingrese su segundo nombre",required=True, widget=forms.TextInput(attrs={"class":"form-control form-control-user"}))
+	ocupacion=forms.ChoiceField(choices=tiposUsuario, required=True)
 	password1=forms.CharField(label="Ingrese su contraseña", required=True, widget=forms.PasswordInput(attrs={"class":"form-control form-control-user"}))
 	password2=forms.CharField(label="Reingrese su contraseña", required=True, widget=forms.PasswordInput(attrs={"class":"form-control form-control-user"}))
 
 	class Meta:
 		model = User
-		fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+		fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'ocupacion']
 	def clean_email(self):
 		email = self.cleaned_data['email']
 
--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login
+from .models import UserClass
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -23,7 +25,11 @@ def register(request):
 
         if user_creation_form.is_valid():
             user_creation_form.save()
-
+            username=user_creation_form.cleaned_data.get('username') #crea una variable con el nombre de usuario sacado del formulario
+            user= User.objects.get(username=username) #instancia un usuario usando el nombre de usuario anterior
+            ocupacion=user_creation_form.cleaned_data.get("ocupacion") #lo mismo que el nombre de usuario pero para la ocupacion
+            user_data = UserClass.objects.create(user=user,ocupacion=ocupacion) #se instancia el modelo userclass usando al usuario y la ocupación declaradas anteriormente
+            user_data.save() #se guardan los datos entregados
             #user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'])
             #login(request, user)
             return redirect('/')
