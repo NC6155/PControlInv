4b51ef7ced7c6322b4130f9c1f75afef0d5eabf0
Nicolas <cariaganicolas8@gmail.com>
2024-12-09 22:35:22 -0300

    test

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+python3 -m pip install -r requirements.txt
 echo "Building the project..."
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
